06ef3d47e083e7cb186e6ad8aed08f853d124d84
Farabi <Farabi@FARABI-PC>
2020-10-01 23:45:32 +0700

    CalculatorGUI

---
+++ CalculatorWindowsFormApp/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace CalculatorWindowsFormApp
+{
+    partial class Layout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNilaiA = new System.Windows.Forms.TextBox();
+            this.txtNilaiB = new System.Windows.Forms.TextBox();
+            this.btnHitung = new System.Windows.Forms.Button();
+            this.lstHasil = new System.Windows.Forms.ListBox();
+            this.cbOperasi = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nilai A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nilai B";
+            // 
+            // txtNilaiA
+            // 
+            this.txtNilaiA.Location = new System.Drawing.Point(108, 76);
+            this.txtNilaiA.Name = "txtNilaiA";
+            this.txtNilaiA.Size = new System.Drawing.Size(100, 20);
+            this.txtNilaiA.TabIndex = 2;
+            // 
+            // txtNilaiB
+            // 
+            this.txtNilaiB.Location = new System.Drawing.Point(108, 117);
+            this.txtNilaiB.Name = "txtNilaiB";
+            this.txtNilaiB.Size = new System.Drawing.Size(100, 20);
+            this.txtNilaiB.TabIndex = 3;
+            // 
+            // btnHitung
+            // 
+            this.btnHitung.Location = new System.Drawing.Point(118, 194);
+            this.btnHitung.Name = "btnHitung";
+            this.btnHitung.Size = new System.Drawing.Size(75, 23);
+            this.btnHitung.TabIndex = 1;
+            this.btnHitung.Text = "Hitung";
+            this.btnHitung.UseVisualStyleBackColor = true;
+            this.btnHitung.Click += new System.EventHandler(this.btnHitung_Click);
+            // 
+            // lstHasil
+            // 
+            this.lstHasil.BackColor = System.Drawing.SystemColors.Menu;
+            this.lstHasil.FormattingEnabled = true;
+            this.lstHasil.Location = new System.Drawing.Point(108, 156);
+            this.lstHasil.Name = "lstHasil";
+            this.lstHasil.Size = new System.Drawing.Size(100, 17);
+            this.lstHasil.TabIndex = 5;
+            // 
+            // cbOperasi
+            // 
+            this.cbOperasi.FormattingEnabled = true;
+            this.cbOperasi.Items.AddRange(new object[] {
+            "Penambahan",
+            "Pengurangan",
+            "Perkalian",
+            "Pembagian"});
+            this.cbOperasi.Location = new System.Drawing.Point(108, 30);
+            this.cbOperasi.Name = "cbOperasi";
+            this.cbOperasi.Size = new System.Drawing.Size(100, 21);
+            this.cbOperasi.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 18);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(41, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Operasi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(41, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 18);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Hasil";
+            // 
+            // Layout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 242);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstHasil);
+            this.Controls.Add(this.cbOperasi);
+            this.Controls.Add(this.btnHitung);
+            this.Controls.Add(this.txtNilaiB);
+            this.Controls.Add(this.txtNilaiA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Layout";
+            this.Text = "Aplikasi Kalkulator Versi Windows";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNilaiA;
+        private System.Windows.Forms.TextBox txtNilaiB;
+        private System.Windows.Forms.Button btnHitung;
+        private System.Windows.Forms.ListBox lstHasil;
+        private System.Windows.Forms.ComboBox cbOperasi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ CalculatorWindowsFormApp/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorWindowsFormApp
+{
+    public partial class Layout : Form
+    {
+        public Layout()
+        {
+            InitializeComponent();
+        }
+
+        private float Penambahan(float a, float b)
+        {
+            return a + b;
+        }
+
+        private float Pengurangan(float a, float b)
+        {
+            return a - b;
+        }
+
+        private float Perkalian(float a, float b)
+        {
+            return a * b;
+        }
+
+        private float Pembagian(float a, float b)
+        {
+            return a / b;
+        }
+
+        private void btnHitung_Click(object sender, EventArgs e)
+        {
+            if (txtNilaiA.Text == "" || txtNilaiB.Text == "" || cbOperasi.SelectedIndex == -1)
+            {
+                string message = "Silahkan Inputkan Nilai dan Pilih Operasi Terlebih Dahulu";
+                string title = "Error";
+                MessageBox.Show(message, title);
+            }
+            else
+            {
+                var a = float.Parse(txtNilaiA.Text); 
+                var b = float.Parse(txtNilaiB.Text); 
+
+                if (cbOperasi.SelectedIndex == 0)
+                {
+                    lstHasil.Items.Clear();
+                    lstHasil.Items.Add(string.Format("{2}", a, b, Penambahan(a, b)));
+                }
+                else if (cbOperasi.SelectedIndex == 1)
+                {
+                    lstHasil.Items.Clear();
+                    lstHasil.Items.Add(string.Format("{2}", a, b, Pengurangan(a, b)));
+                }
+                else if (cbOperasi.SelectedIndex == 2)
+                {
+                    lstHasil.Items.Clear();
+                    lstHasil.Items.Add(string.Format("{2}", a, b, Perkalian(a, b)));
+                }
+                else if (cbOperasi.SelectedIndex == 3)
+                {
+                    lstHasil.Items.Clear();
+                    lstHasil.Items.Add(string.Format("{2}", a, b, Pembagian(a, b)));
+                }
+            }
+        }
+    }
+}
